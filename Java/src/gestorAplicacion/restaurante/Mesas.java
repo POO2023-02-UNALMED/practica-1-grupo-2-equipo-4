7c2d7cefefd5a893035cd69ab080a4480e8b78d6
Juanfepx <juanfepx@gmail.com>
2023-10-07 18:33:03 -0500

    setter y getter mesa

--- Java/src/gestorAplicacion/restaurante/Mesas.java
+++ Java/src/gestorAplicacion/restaurante/Mesas.java
@@ -27,5 +27,37 @@ public class Mesas implements Serializable {
     public void liberarMesa() {
 
     }
+
+    public static ArrayList<Mesas> getMesas() {
+        return mesas;
+    }
+
+    public void setIdMesa(int idMesa) {
+        this.idMesa = idMesa;
+    }
+
+    public int getNumeroDeSillas() {
+        return numeroDeSillas;
+    }
+
+    public void setNumeroDeSillas(int numeroDeSillas) {
+        this.numeroDeSillas = numeroDeSillas;
+    }
+
+    public boolean isEstadoMesa() {
+        return estadoMesa;
+    }
+
+    public void setEstadoMesa(boolean estadoMesa) {
+        this.estadoMesa = estadoMesa;
+    }
+
+    public int getIdCliente() {
+        return idCliente;
+    }
+
+    public void setIdCliente(int idCliente) {
+        this.idCliente = idCliente;
+    }
 }
 
