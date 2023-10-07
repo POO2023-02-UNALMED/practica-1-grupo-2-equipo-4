3ef0872f597527949d4beb65927fd20c52f61e6c
Nicorb72 <141965721+Nicorb72@users.noreply.github.com>
2023-10-07 16:38:51 -0500

    Factura
    Corrección a atributos de la clase

--- Java/src/gestorAplicacion/administracion/Factura.java
+++ Java/src/gestorAplicacion/administracion/Factura.java
@@ -1,19 +1,19 @@
 package gestorAplicacion.administracion;
 
+import gestorAplicacion.restaurante.Mesas;
 import gestorAplicacion.restaurante.Pedido;
 
 public class Factura {
     public int idEmpleado;
-    public int idMesa;
-    public Pedido tomarPedido;
+    public Mesas mesa;
+    public Pedido pedido;
     public int fecha;
     public int idFactura;
 
-    public Factura(int idEmpleado, idMesa, Pedido tomarPedido, int fecha, idFactura) {
+    public Factura(int idEmpleado, Mesas mesa, Pedido tomarPedido, int idFactura) {
     	this.idEmpleado = idEmpleado;
-    	this.idMesa = idMesa;
-    	this.Pedido = tomarPedido;
-    	this.fecha = fecha;
+    	this.mesa = mesa;
+    	this.pedido = tomarPedido;
     	this.idFactura = idFactura;
     }
     
