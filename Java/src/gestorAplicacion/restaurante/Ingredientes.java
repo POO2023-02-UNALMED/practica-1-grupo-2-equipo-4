58a6bc1a92f5b96530932489b97e3a5c2d8e4fdd
Nicorb72 <141965721+Nicorb72@users.noreply.github.com>
2023-10-07 15:36:06 -0500

    Ingredientes
    Cambio de nombre de la clase

--- Java/src/gestorAplicacion/restaurante/Ingredientes.java
+++ Java/src/gestorAplicacion/restaurante/Ingredientes.java
@@ -3,14 +3,13 @@ package gestorAplicacion.restaurante;
 import java.lang.reflect.Array;
 import java.util.ArrayList;
 
-public class Inventario {
+public class Ingredientes {
     public String nombreIngrediente;
     public int precio;
     public int cantidad;
     public boolean disponibilidadInventario;
     public ArrayList<String> listaIngredientes;
-
-    public Inventario() {
+    public Ingredientes () {
     }
 
     public int getCantidad() {
