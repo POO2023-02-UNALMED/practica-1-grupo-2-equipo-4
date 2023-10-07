f3c7c23cd9db563c7dd6ced41c45c51587840575
Criiper <perezloperacristiandavid@gmail.com>
2023-10-07 15:55:29 -0500

    Constructor, setter y getter en Calificacion

--- Java/src/gestorAplicacion/administracion/Calificacion.java
+++ Java/src/gestorAplicacion/administracion/Calificacion.java
@@ -1,13 +1,40 @@
 package gestorAplicacion.administracion;
 
 public class Calificacion {
-    public int idFactura;
-    public int idEmpleado;
+    private int idFactura;
+    private int idEmpleado;
     private int calificacion;
 
-
+    
+    public Calificacion(int idFactura, int idEmpleado, int calificacion) {
+    	this.idFactura = idFactura;
+    	this.idEmpleado = idEmpleado;
+    	this.calificacion = calificacion;
+    }
+    
+    public void setIdFactura(int idFactura){
+    	this.idFactura = idFactura;
+    }
+    
+    public void setIdEmpleado(int idEmpleado){
+    	this.idEmpleado = idEmpleado;
+    }
+    
+    
     public void setCalificacion(int calificacion) {
         this.calificacion = calificacion;
+    }
+    
+    public int getIdFactura() {
+    	return idFactura;
+    }
+    
+    public int getIdEmpleado() {
+    	return idEmpleado;
+    }
+    
+    public int getCalificacion() {
+    	return calificacion;
     }
 }
 
