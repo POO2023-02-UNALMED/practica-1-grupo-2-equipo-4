333a7404610eae066ecd5a4336641e302725c594
Juanfepx <juanfepx@gmail.com>
2023-10-07 15:53:29 -0500

    Contructor "Mesas" y esqueleto de los metodos de dicha clase

--- Java/src/gestorAplicacion/restaurante/Mesas.java
+++ Java/src/gestorAplicacion/restaurante/Mesas.java
@@ -1,10 +1,26 @@
 package gestorAplicacion.restaurante;
 
-public class Mesas {
+public class Mesas{
     public int idMesa;
     public int numeroDeSillas;
     public boolean estadoMesa;
     public int idCliente;
+   public Mesas(int idMesa,int numeroDeSillas,boolean estadoMesa,int idCliente){
+       this.idMesa = idMesa;
+       this.numeroDeSillas = numeroDeSillas;
+       this.estadoMesa = estadoMesa;
+       this.idCliente = idCliente;
+   }
+   public void crearReserva(){
+
+
+   }
+    public void cancelarReserva(){
+
+    }
+    public void liberarMesa(){
+
+    }
 
 
 }
