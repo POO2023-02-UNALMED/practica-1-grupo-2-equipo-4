03529c622ee351c410f4b1bce473e8c091e7dc64
Nicorb72 <141965721+Nicorb72@users.noreply.github.com>
2023-10-07 18:20:49 -0500

    Mesas
    Correcciones y Método Static con lista de objetos mesas

--- Java/src/gestorAplicacion/restaurante/Mesas.java
+++ Java/src/gestorAplicacion/restaurante/Mesas.java
@@ -1,27 +1,31 @@
 package gestorAplicacion.restaurante;
-
+import java.util.ArrayList;
 import java.io.Serializable;
-
 public class Mesas implements Serializable {
+    public static java.util.ArrayList<Mesas> mesas;
     public int idMesa;
     public int numeroDeSillas;
     public boolean estadoMesa;
     public int idCliente;
-    public Mesas(int idMesa,int numeroDeSillas,boolean estadoMesa,int idCliente){
+
+    public Mesas(int idMesa, int numeroDeSillas, boolean estadoMesa, int idCliente) {
         this.idMesa = idMesa;
         this.numeroDeSillas = numeroDeSillas;
         this.estadoMesa = estadoMesa;
         this.idCliente = idCliente;
+        mesas.add(this);
     }
-   public void crearReserva(){
 
-   }
-    public void cancelarReserva(){
-
-    }
-    public void liberarMesa(){
+    public void crearReserva() {
 
     }
 
+    public void cancelarReserva() {
 
+    }
+
+    public void liberarMesa() {
+
+    }
 }
+
