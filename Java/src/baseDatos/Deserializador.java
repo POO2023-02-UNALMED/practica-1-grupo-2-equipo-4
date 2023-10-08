df04dce2926a5d9b4ab8e6360c9328bb4af380e3
Tyrussgc <141864114+Tyrussgc@users.noreply.github.com>
2023-10-07 19:37:52 -0500

    Clases, Deserializador y Serializador

--- Java/src/baseDatos/Deserializador.java
+++ Java/src/baseDatos/Deserializador.java
@@ -1,4 +1,9 @@
-package baseDatos.temp;
+package baseDatos;
+import  java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.ObjectOutputStream;
+
 
 public class Deserializador {
 }
--- Java/src/baseDatos/Serializador.java
+++ Java/src/baseDatos/Serializador.java
@@ -1,4 +1,10 @@
-package baseDatos.temp;
-
+package baseDatos;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.io.PrintWriter;
 public class Serializador {
+
 }
