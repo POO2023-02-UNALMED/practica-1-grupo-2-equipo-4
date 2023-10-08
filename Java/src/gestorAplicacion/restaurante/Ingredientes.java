2855c6c5615f3ed48f494631c52f00672f5af82b
Nicorb72 <141965721+Nicorb72@users.noreply.github.com>
2023-10-07 19:46:19 -0500

    Ingredientes
    Creación del constructor y métodos de la clase ingredientes, junto con el método de comprar ingredientes que agrega ingredientes y agrega a los gastos el valor de todos los ingredientes comprados

--- Java/src/gestorAplicacion/restaurante/Ingredientes.java
+++ Java/src/gestorAplicacion/restaurante/Ingredientes.java
@@ -1,22 +1,50 @@
 package gestorAplicacion.restaurante;
 
+import gestorAplicacion.administracion.Contabilidad;
+
 import java.io.Serializable;
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 
 public class Ingredientes implements Serializable {
+    public static ArrayList<Ingredientes> listaIngredientes;
     public String nombreIngrediente;
-    public int precio;
+    public float precio;
     public int cantidad;
-    public boolean disponibilidadInventario;
-    public ArrayList<String> listaIngredientes;
-    public Ingredientes () {
+    public boolean disponibilidad;
+    public Ingredientes (String nombreIngrediente, float precio, int cantidad, boolean disponibilidad) {
+        this.nombreIngrediente = nombreIngrediente;
+        this.precio = precio;
+        this.cantidad = cantidad;
+        this.disponibilidad = disponibilidad;
+        listaIngredientes.add(this);
     }
 
     public int getCantidad() {
         return cantidad;
     }
 
+    public void setCantidad (int cantidad){
+        this.cantidad = cantidad;
+    }
 
+    public float getPrecio() {
+        return precio;
+    }
 
+    public void setPrecio(float precio) {
+        this.precio = precio;
+    }
+
+    public boolean isDisponibilidad() {
+        return disponibilidad;
+    }
+
+    public void setDisponibilidad(boolean disponibilidad){
+        this.disponibilidad = disponibilidad;
+    }
+
+    public void comprarIngredientes (int cantidad){ //Método para comprar ingredientes
+        this.cantidad += cantidad; // se agregan los ingredientes comprados
+        Contabilidad.agregarGasto(cantidad * this.precio); // se suma a los gastos la cantidad comprada por el precio del ingrediente
+    }
 }
