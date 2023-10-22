3c7d13ca77cb2de56ca8019d852ed49339df0319
Tyrussgc <141864114+Tyrussgc@users.noreply.github.com>
2023-10-22 04:06:10 -0500

    Contabilidad: Metodos, Atributos y anotaciones. xdddddd

--- Java/src/gestorAplicacion/administracion/Contabilidad.java
+++ Java/src/gestorAplicacion/administracion/Contabilidad.java
@@ -2,28 +2,74 @@ package gestorAplicacion.administracion;
 
 import java.io.Serializable;
 import java.util.ArrayList;
+import gestorAplicacion.restaurante.*;
 
 public class Contabilidad implements Serializable{
-    public static float saldo = 1000000;
-    public ArrayList<Factura> Facturas;
-    public static final float serviciosPublicos = 0;
+    private static final long serialVersionUID = 1L;
+    private Empleado empleado;
+    private double calcularUtilidad;
+    private double calcularIngresos;
+    private double pagarSueldos;
+    private double calcularGastos;
+    private static float saldo = 1000000;
+    private ArrayList<Factura> facturas;
+    private static final float serviciosPublicos = 0;
     
-    public Contabilidad(float saldo,ArrayList<Factura> Facturas){
+    public Contabilidad(float saldo,ArrayList<Factura> facturas){
         Contabilidad.saldo = saldo;
-        this.Facturas = Facturas;
-
-
+        this.facturas = facturas;
     }
-
     public static float getSaldo() {
         return saldo;
     }
-
     public static void setSaldo(float saldo) {
         Contabilidad.saldo = saldo;
     }
-
     public ArrayList<Factura> getFacturas() {
-        return Facturas;
+        return facturas;
+    }
+    // Método para pagar servicios No terminado
+    public void pagarServicios(float serviciosPublicos) {
+        if (serviciosPublicos > 0 && saldo >= serviciosPublicos) {
+            saldo -= serviciosPublicos;
+            System.out.println("Pago de servicios exitoso. Nuevo saldo: " + saldo);
+        } else {
+            System.out.println("Fondos insuficientes para realizar el pago.");
+        }
+    }
+    // Metodo para calcular Ingresos Totales
+    public double calcularIngresos() {
+        double ingresoVentas = 0;
+        for (Factura factura : facturas) {
+            ingresoVentas += factura.precioTotal();
+        }
+        this.calcularIngresos = ingresoVentas;
+        return this.calcularIngresos;
+    }
+    // Metodo para calular Utilidad
+    public double calcularUtilidad() {
+        double totalGastos = calcularGastos() + pagarSueldos();
+        double ingresoVentas = calcularIngresos();
+        this.calcularUtilidad = ingresoVentas - totalGastos;
+        return this.calcularUtilidad;
+    }
+    // Metodo para calcular Los Gastos de La Hamburgueseria
+    public double calcularGastos(){
+
+        return calcularGastos;
+    }
+    // Metodo para Pagar los sueldos a los empleados
+    public double pagarSueldos() {
+        double totalPago = 0;
+        for (Empleado empleado : getEmpleado()) { // deberia
+            totalPago += empleado.getSalario();
+            if (empleado.Bono()) { // Si el Empleado tiene un buen promedio de las calificaciones se le aplicara el Bono a su salario final.
+                totalPago += (empleado.getSalario() * empleado.getValorBono()); // Necesita multiplicar el Salario por el valor del bono.
+            } else {
+                totalPago += empleado.getSalario();
+            }
+        }
+        this.pagarSueldos = totalPago;
+        return this.pagarSueldos;
     }
 }
