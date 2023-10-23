37442582a9abd62c465e79186a7a9cf66bd4822b
David852002 <ddelgadoo@unal.edu.co>
2023-10-23 00:35:44 -0500

    main objetos

--- Java/src/uiMain/Main.java
+++ Java/src/uiMain/Main.java
@@ -1,7 +1,6 @@
 package uiMain;
 
 
-
 import gestorAplicacion.restaurante.Ingredientes;
 import gestorAplicacion.restaurante.Mesas;
 import gestorAplicacion.restaurante.Pedido;
@@ -15,10 +14,10 @@ public class Main {
 
         // gaseosas
 
-        Gaseosas coca_cola = new Gaseosas("Coca cola",4000,20);
-        Gaseosas Sprite = new Gaseosas("Sprite",3500,20);
+        Gaseosas coca_cola = new Gaseosas("Coca cola", 4000, 20);
+        Gaseosas Sprite = new Gaseosas("Sprite", 3500, 20);
         Gaseosas premio = new Gaseosas("Premio", 3000, 20);
-        Gaseosas quatro= new Gaseosas("Quatro", 2500, 20);
+        Gaseosas quatro = new Gaseosas("Quatro", 2500, 20);
 
 
         // ingredientes
@@ -27,22 +26,27 @@ public class Main {
         Ingredientes carneDePollo = new Ingredientes("Carne de pollo", 7000, 20);
         Ingredientes carneVegana = new Ingredientes("Carne vegana", 4000, 20);
         Ingredientes tomate = new Ingredientes("Tomate", 400, 20);
-        Ingredientes cebolla = new Ingredientes("Cebolla", 200,20);
-        Ingredientes lechuga = new Ingredientes("Lechuga",150, 20);
+        Ingredientes cebolla = new Ingredientes("Cebolla", 200, 20);
+        Ingredientes lechuga = new Ingredientes("Lechuga", 150, 20);
         Ingredientes queso = new Ingredientes("Queso", 2000, 20);
-        Ingredientes tocineta = new Ingredientes("Tocineta", 1000, 20 );
+        Ingredientes tocineta = new Ingredientes("Tocineta", 1000, 20);
 
         // comidas
-        Ingredientes[] ingredientesClasicaCarne = {pan,carneDeRes, tomate,cebolla,lechuga};
-        int[] cantidadesClasicaCarne ={2, 1,1,1,1};
+        Ingredientes[] ingredientesClasicaCarne = {pan, carneDeRes, tomate, cebolla, lechuga};
+        int[] cantidadesClasicaCarne = {2, 1, 1, 1, 1};
         Comida clasicaDeCarne = new Comida("Clasica de carne", ingredientesClasicaCarne, cantidadesClasicaCarne);
 
-        Ingredientes[] ingredientesCarneQuesoYTocineta = {pan,carneDeRes,queso, tocineta, tomate, cebolla, lechuga};
-        int[] cantidadesCarneQuesoYTocineta = {2,1,2,3,1,2,2};
+        Ingredientes[] ingredientesCarneQuesoYTocineta = {pan, carneDeRes, queso, tocineta, tomate, cebolla, lechuga};
+        int[] cantidadesCarneQuesoYTocineta = {2, 1, 2, 3, 1, 2, 2};
         Comida EspecialQuesoYTocineta = new Comida("Especial con queso y tocineta", ingredientesCarneQuesoYTocineta, cantidadesCarneQuesoYTocineta);
 
-        Ingredientes [] ingredientesDobleCarneTocineta = {pan , carneDeRes, queso, tocineta, tomate, cebolla, lechuga};
-        int [] 
+        Ingredientes[] ingredientesDobleCarneTocineta = {pan, carneDeRes, queso, tocineta, tomate, cebolla, lechuga};
+        int[] cantidadesDobleCarneTocineta = {2,2,3,4,2,2,2};
+        Comida DobleCarneTocineta = new Comida("Doble carne tocineta", ingredientesDobleCarneTocineta,cantidadesDobleCarneTocineta);
+
+        Ingredientes[] ingredientesVegetariana = {pan, carneVegana, queso, tomate, cebolla, lechuga};
+        int [] cantidadesVegetariana = {2,1,2,2,2,2};
+        Comida vegetariana = new Comida()
 
 
 
@@ -64,18 +68,15 @@ public class Main {
         System.out.println(mesa2.toString());
 */
 
-     Mesas mesa1 = new Mesas(1,4);
-     Mesas mesa2 = new Mesas(2,4);
-     Mesas mesa3 = new Mesas(3,7);
-     Mesas mesa4 = new Mesas(4,8);
-     Mesas mesa5 = new Mesas(5,10);
-     Mesas mesa6 = new Mesas(6,10);
+        Mesas mesa1 = new Mesas(1, 4);
+        Mesas mesa2 = new Mesas(2, 4);
+        Mesas mesa3 = new Mesas(3, 7);
+        Mesas mesa4 = new Mesas(4, 8);
+        Mesas mesa5 = new Mesas(5, 10);
+        Mesas mesa6 = new Mesas(6, 10);
 
-     mesa1.crearReserva(789, 1, "11-12-2023 14:00:00");
-     mesa2.crearReserva(456, 2, "11-12-2023 14:00:00");
-
-
-
+        mesa1.crearReserva(789, 1, "11-12-2023 14:00:00");
+        mesa2.crearReserva(456, 2, "11-12-2023 14:00:00");
 
 
     }
