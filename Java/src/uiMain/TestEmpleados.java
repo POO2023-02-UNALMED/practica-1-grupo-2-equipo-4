a283ce1c536a32fc61a2c52be2ba2431b56d443d
Nicorb72 <141965721+Nicorb72@users.noreply.github.com>
2023-10-24 12:53:07 -0500

    -test empleados

--- Java/src/uiMain/TestEmpleados.java
+++ Java/src/uiMain/TestEmpleados.java
@@ -82,51 +82,6 @@ public class TestEmpleados implements Serializable {
         Mesas mesa15 = new Mesas(15, 10);
 
 
-        Cocinero cocinero = new Cocinero("Raúl",2,200000);
-        System.out.println("El Empleado "+ cocinero.getNombre() +" Es un "+cocinero.getOcupacion());
-        Mesero mesero1 = new Mesero("Pablo",20000);
-        Mesero mesero2 = new Mesero("Sofia",20000);
-        Mesero mesero3 = new Mesero("Andres",20000);
-        Mesero mesero4 = new Mesero("Monica",20000);
-        Mesero mesero5 = new Mesero("Horacio",20000);
-        Empleado mesero6 = new Mesero("Jacinto",20000);
-        Empleado cocinero1 = new Cocinero("Juan",9,"Carnes");
-        mesero1.agregarMesas(mesa1);
-        mesero1.agregarMesas(mesa2);
-        mesero1.agregarMesas(mesa3);
-        mesero1.agregarMesas(mesa4);
-        mesero2.agregarMesas(mesa5);
-        mesero2.agregarMesas(mesa6);
-        mesero2.agregarMesas(mesa7);
-        mesero2.agregarMesas(mesa8);
-        mesero3.agregarMesas(mesa9);
-        mesero3.agregarMesas(mesa10);
-        mesero3.agregarMesas(mesa11);
-        mesero3.agregarMesas(mesa12);
-        mesero4.agregarMesas(mesa13);
-        mesero4.agregarMesas(mesa14);
-        mesero4.agregarMesas(mesa15);
-        mesero4.agregarMesas(mesa1);
-        mesero5.agregarMesas(mesa2);
-        mesero5.agregarMesas(mesa3);
-        mesero5.agregarMesas(mesa4);
-        mesero5.agregarMesas(mesa5);
-
-        System.out.println(mesero1.toString());
-        System.out.println(mesero2.toString());
-        System.out.println(mesero3.toString());
-        System.out.println(mesero4.toString());
-        System.out.println(mesero6.toString());
-
-        System.out.println(mesero6.accion());
-        System.out.println(mesero6.getOcupacion());
-        System.out.println(cocinero1.accion());
-        System.out.println(cocinero1.getOcupacion());
-        System.out.println(mesero2.accion());
-        System.out.println(mesero2.getOcupacion());
-        System.out.println(mesero3.accion());
-        System.out.println(mesero3.getOcupacion());
-
         ArrayList<Mesas> mesas = new ArrayList<>();
         mesas.add(mesa1);
         mesas.add(mesa2);  // Serializar las calificaciones en un archivo
